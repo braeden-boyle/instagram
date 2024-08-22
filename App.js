3732b213f65c43fcfeeb0908f7b65129825a4afb
braeden-boyle <2braedenboyle@gmail.com>
2024-08-21 22:46:44 -0400

    properly implemented a different screen for the Add tab

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ import LandingScreen from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register';
 import LoginScreen from './components/auth/Login';
 import MainScreen from './components/Main';
-import Main from './components/Main';
+import AddScreen from './components/main/Add'
 
 const Stack = createStackNavigator();
 
@@ -80,6 +80,7 @@ export class App extends Component {
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Main">
             <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
+            <Stack.Screen name="Add" component={AddScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import { connect } from 'react-redux'
@@ -9,10 +8,13 @@ import { fetchUser } from '../redux/actions'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import FeedScreen from './main/Feed'
-import AddScreen from './main/Add'
 import ProfileScreen from './main/Profile'
 
 const Tab = createBottomTabNavigator();
+
+const EmptyScreen = () => {
+    return (null)
+}
 
 export class Main extends Component {
     componentDidMount() {
@@ -27,16 +29,24 @@ export class Main extends Component {
                         <MaterialCommunityIcons name='home' color={color} size={26}/>
                     )
                 }} />
-                <Tab.Screen name="Add" component={AddScreen} options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name='plus-box' color={color} size={26}/>
-                    )
-                }} />
+                <Tab.Screen name="Add" component={EmptyScreen}
+                    listeners={({ navigation }) => ({
+                        tabPress: event => {
+                            event.preventDefault();
+                            navigation.navigate("Add")
+                        }
+                    })} 
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name='plus-box' color={color} size={26}/>
+                        )
+                    }} 
+                />
                 <Tab.Screen name="Profile" component={ProfileScreen} options={{
                     headerShown: false,
                     tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name='account' color={color} size={26}/>
+                        <MaterialCommunityIcons name='account-circle' color={color} size={26}/>
                     )
                 }} />
             </Tab.Navigator>
