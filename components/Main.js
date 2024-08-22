8b53c1adf129c156d9d37e97febd0dbe014904a8
braeden-boyle <2braedenboyle@gmail.com>
2024-08-21 22:56:21 -0400

    changed bottom tab navigator to MaterialBottomTabNavigator for the features

--- components/Main.js
+++ components/Main.js
@@ -5,12 +5,12 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { fetchUser } from '../redux/actions'
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
 
 import FeedScreen from './main/Feed'
 import ProfileScreen from './main/Profile'
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const EmptyScreen = () => {
     return (null)
@@ -22,14 +22,14 @@ export class Main extends Component {
     }
     render() {
         return (
-            <Tab.Navigator>
+            <Tab.Navigator initialRouteName="Feed" labeled={false}>
                 <Tab.Screen name="Feed" component={FeedScreen} options={{
                     headerShown: false,
                     tabBarIcon: ({ color, size }) => (
                         <MaterialCommunityIcons name='home' color={color} size={26}/>
                     )
                 }} />
-                <Tab.Screen name="Add" component={EmptyScreen}
+                <Tab.Screen name="AddContainer" component={EmptyScreen}
                     listeners={({ navigation }) => ({
                         tabPress: event => {
                             event.preventDefault();
