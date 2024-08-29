021c4caccbd7dca435e5411ec75cc599d2e1bb1c
braeden-boyle <2braedenboyle@gmail.com>
2024-08-29 17:48:29 -0400

    Created Redux action and etc for fetching user posts

--- components/Main.js
+++ components/Main.js
@@ -3,7 +3,7 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { fetchUser } from '../redux/actions'
+import { fetchUser, fetchUserPosts } from '../redux/actions'
 
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
 
@@ -19,6 +19,7 @@ const EmptyScreen = () => {
 export class Main extends Component {
     componentDidMount() {
         this.props.fetchUser();
+        this.props.fetchUserPosts();
     }
     render() {
         return (
@@ -57,6 +58,6 @@ export class Main extends Component {
 const mapStateToProps = (store) => ({
     currentUser: store.userState.currentUser
 })
-const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser}, dispatch);
+const mapDispatchProps = (dispatch) => bindActionCreators({ fetchUser, fetchUserPosts }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchProps)(Main);
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,6 +1,6 @@
-import { getDoc, doc } from "firebase/firestore";
+import { getDoc, doc, collection, query, orderBy, getDocs } from "firebase/firestore";
 import { db, auth } from "../../firebaseConfig";
-import { USER_STATE_CHANGE } from "../constants";
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE } from "../constants";
 
 export function fetchUser() {
     return async (dispatch) => {
@@ -11,7 +11,7 @@ export function fetchUser() {
             if (docSnap.exists()) {
                 dispatch({
                     type: USER_STATE_CHANGE,
-                    currentUser : docSnap.data(),
+                    currentUser: docSnap.data(),
                 });
             } else {
                 console.log('No such document');
@@ -20,6 +20,28 @@ export function fetchUser() {
             }
         } catch (error) {
             console.error('Error fetching user: ', error);
+        }
+    };
+}
+
+export function fetchUserPosts() {
+    return async (dispatch) => {
+        try {
+            const q = query(collection(db, `posts/${auth.currentUser.uid}/userPosts`), orderBy('createdAt', 'asc'));
+            const querySnapshot = await getDocs(q)
+            
+            let posts = querySnapshot.docs.map(doc => {
+                const data = doc.data();
+                const id = doc.id;
+                return { id, ...data }
+            })
+            console.log(posts);
+            dispatch({
+                type: USER_POSTS_STATE_CHANGE,
+                posts
+            });
+        } catch (error) {
+            console.error('Error fetching posts: ', error);
         }
     };
 }
