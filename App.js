ba88310d8c1dcacd8047973dfe8b64a2a200f144
braeden-boyle <2braedenboyle@gmail.com>
2024-08-29 17:59:32 -0400

    Modified Redux store configuration to allow post map to be put into the state

--- App.js
+++ App.js
@@ -9,9 +9,12 @@ import { Provider } from 'react-redux';
 import rootReducer from './redux/reducers'
 import { thunk } from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
+
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  }).concat(thunk)
 });
 
 if(getApps().length < 1){
