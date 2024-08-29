661b906e6b3b1c2c98da7cf98ba15fdb53821257
braeden-boyle <2braedenboyle@gmail.com>
2024-08-28 20:14:38 -0400

    Implemented saving post data in Firebase database

--- components/main/Save.js
+++ components/main/Save.js
@@ -2,10 +2,21 @@ import React, { useState } from 'react'
 import { View, TextInput, Image, Button } from 'react-native'
 import { auth, db, firebaseApp } from '../../firebaseConfig'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { addDoc, collection, doc, serverTimestamp } from 'firebase/firestore'
 
 
-export default function Save(props) {
+export default function Save(props, { navigation }) {
     const [caption, setCaption] = useState('')
+
+    const savePostData = async (downloadURL) => {
+        const docRef = await addDoc(collection(db, `posts/${auth.currentUser.uid}/userPosts`), {
+            downloadURL: downloadURL,
+            caption: caption,
+            createdAt: serverTimestamp()
+        }).then(() => {
+            props.navigation.popToTop();
+        });
+    }
 
     const uploadImage = async () => {
         const uri = props.route.params.image;
@@ -28,11 +39,12 @@ export default function Save(props) {
                 console.log(`Upload is ${progress}% done`);
             },
             (error) => {
-                console.error('Uplaod failed', error)
+                console.error('Upload failed', error)
             },
             // Upload completed
             () => {
                 getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    savePostData(downloadURL);
                     console.log('File available at ', downloadURL )
                 });
             }
