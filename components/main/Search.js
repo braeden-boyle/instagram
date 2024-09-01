56b6d2dc6af74562a8d556a8027fc65528a16344
braeden-boyle <2braedenboyle@gmail.com>
2024-09-01 17:37:17 -0400

    created fetchUsers function

---
+++ components/main/Search.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, FlatList } from 'react-native'
+
+import { db } from '../../firebaseConfig'
+import { collection, query, snapshotEqual } from 'firebase/firestore'
+
+export default function Search() {
+    const [users, setUsers] = useState([])
+
+    const fetchUsers = async (search) => {
+        const q = query(collection(db, 'users'), where ('name', '>=', search));
+        const snapshot = await getDocs(q)
+        .then((snapshot) => {
+            let users = snapshot.docs.map(doc => {
+                const data = doc.data();
+                const id = doc.id;
+                return { id, ...data }
+            });
+            setUsers(users);
+        })
+    }
+
+    return (
+        <View>
+            <Text>Feed</Text>
+        </View>
+  )
+}
